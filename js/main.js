ed4d6a2af7605572e40b427aa85ece9c9195c314
GitHubUzr <49741950+GitHubUzr@users.noreply.github.com>
2021-03-19 21:30:55 -0400

    Updated detail page

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
+var data = {
+  datasets: [{
+      data: [7, 32, 61],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(54, 162, 235, 0.2)'
+    ],
+  }],
+
+  // These labels appear in the legend and in the tooltips when hovering different arcs
+  labels: [
+      'Fat',
+      'Carbs',
+      'Protein'
+  ]
+};
+
 var ctx = document.getElementById("myChart");
 var myChart = new Chart(ctx, {
   type: 'pie',
-  data: {
-    datasets: [{
-        data: [3.5, 17, 32],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(54, 162, 235, 0.2)'
-      ],
-    }],
-
-    // These labels appear in the legend and in the tooltips when hovering different arcs
-    labels: [
-        'Fat',
-        'Carbs',
-        'Protein'
-    ]
-  },
+  data: data,
   options: {responsive:false},
 });
